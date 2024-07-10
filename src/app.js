b01a6f05d0c1fb0e99ee7d593edc64a200441daf
Miguel Navas <143790463+menr-25@users.noreply.github.com>
2024-07-10 14:55:46 +0000

    js finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuseGenerator() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class.",
+      "when I was sleeping.",
+      "while I was exercising.",
+      "during my lunch.",
+      "while I was praying."
+    ];
+
+    function randomElement(array) {
+      return array[Math.floor(Math.random() * array.length)];
+    }
+
+    let excuse = `${randomElement(who)} ${randomElement(
+      action
+    )} ${randomElement(what)} ${randomElement(when)}`;
+
+    return excuse;
+  }
+
+  function changeText() {
+    let paragraph = document.getElementById("excuse");
+    paragraph.innerText = excuseGenerator();
+  }
+
+  document.getElementById("button").addEventListener("click", changeText);
 };
